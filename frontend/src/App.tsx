776da29a7dde99e9afa114b52480bb90169f1460
ruanweiguang <weiguang.ruan@shengyc.com>
2025-09-17 19:27:34 +0800

    feat: 初始化项目结构，添加基本组件和样式

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Canvas from "./Canvas/Canvas";
+
+function App() {
+  return <Canvas width={800} height={800} />;
+}
+
+export default App;
