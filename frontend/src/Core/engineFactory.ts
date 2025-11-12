253d9a71c9a26105b4bcb7742454dc4df44a61bc
baiyuze <1347417308@qq.com>
2025-11-12 12:04:16 +0800

    feat: 添加 createCanvasKit 函数以初始化 CanvasKit，支持不同环境的文件定位

--- frontend/src/Core/engineFactory.ts
+++ frontend/src/Core/engineFactory.ts
@@ -13,6 +13,7 @@ import { DragSystem } from "./System/DragSystem";
 import { ZoomSystem } from "./System/ZoomSystem";
 import { ScrollSystem } from "./System/ScrollSystem";
 import type { DefaultConfig } from "./types";
+import CanvasKitInit from "canvaskit-wasm";
 
 export async function createEngine(dsls: any[], defaultConfig: DefaultConfig) {
   const core = new Core(dsls);
@@ -33,4 +34,32 @@ export async function createEngine(dsls: any[], defaultConfig: DefaultConfig) {
   engine.addSystem(new SelectionSystem(ctx, engine));
 
   return engine;
+}
+
+export async function createCanvasKit() {
+  let CanvasKit;
+  const locateFile = (url: string, file: string) => {
+    return url + file;
+  };
+
+  try {
+    CanvasKit = await CanvasKitInit({
+      locateFile: (file) =>
+        locateFile(
+          "https://cdn.bootcdn.net/ajax/libs/canvaskit-wasm/0.40.0/",
+          file
+        ),
+    });
+  } catch (error) {
+    const url =
+      import.meta.env?.MODE === "production"
+        ? "/design/canvaskit/"
+        : "/node_modules/canvaskit-wasm/bin/";
+    CanvasKit = await CanvasKitInit({
+      locateFile: (file) => locateFile(url, file),
+    });
+    return CanvasKit;
+  }
+
+  return CanvasKit;
 }
